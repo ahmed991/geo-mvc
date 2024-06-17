7f5e7721aaeea9706674e2016c23d7c62bc2ee4f
ahmadhanif946 <ahmadhanif946@gmail.com>
2024-06-17 21:17:54 +0200

    sync map and barchart and pie chart

--- app/Application.js
+++ app/Application.js
@@ -18,8 +18,15 @@ window.appdata = {
     appName: 'CityApp',
     cityName: 'Utrecht',
     cityCoords: [133500, 455200],
-    zoom: 16.6
+    zoom: 6.6,
+    wfsUrl: null,
+    baseUrl: 'https://gisedu.itc.utwente.nl/cgi-bin/mapserv.exe?',
+    mapfile: 'd:/iishome/student/s3182363/gpw/CityApp/api/adminboundaries.map' 
 };
+appdata.wfsUrl = appdata.baseUrl + 'MAP=' + appdata.mapfile +
+	'&SERVICE=WFS&version=1.1.0&REQUEST=GetFeature' +
+	'&outputformat=geojson&srsname=EPSG:28992';
+/* --- */
 /* --- */
 
 /* Initialization function */
